108486f22dcaeec7fa2b3dd1b0edb0e132d2a30c
Sue Burt <sookburt@gmail.com>
2022-03-03 17:03:21 +0000

    Refactored test to shorten it

--- romanNumerals.test.js
+++ romanNumerals.test.js
@@ -2,16 +2,8 @@ const { getRomanNumeral } = require("./romanNumerals");
 
 describe("getRomanNumeral", () => {
   test("returns 1 when I passed in", () => {
-
-    // Arrange
-    const I = "I";
-    const expectedResult = 1;
-
-    // Act
-    const actualResult = getRomanNumeral(I);
-
-    // assert
-    expect(actualResult).toBe(expectedResult);
+    // Arrange : Act : Assert
+    expect(getRomanNumeral("I")).toBe(1);
 
   })
 });
